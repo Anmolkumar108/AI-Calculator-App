3783cfbd3bf20062fdc833e9a854c2f9439899a4
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-05-20 22:18:13 +0530

    update

--- Calculator-App/pages/area.py
+++ Calculator-App/pages/area.py
@@ -49,4 +49,53 @@ def create_area_calculator(content):
             "Triangle",
             "Parallelogram"
         ],
+        variable=shape_var,
+        width=300,
+        height=45,
+        state="readonly",
+        command=lambda x: update_placeholders()
+    )
+
+    shape_menu.pack(pady=15)
+
+    # =========================
+    # ENTRY 1
+    # =========================
+    entry1 = ctk.CTkEntry(
+        main_scroll,
+        width=350,
+        height=50,
+        font=("Arial", 18)
+    )
+
+    entry1.pack(pady=10)
+
+    # =========================
+    # ENTRY 2
+    # =========================
+    entry2 = ctk.CTkEntry(
+        main_scroll,
+        width=350,
+        height=50,
+        font=("Arial", 18)
+    )
+
+    entry2.pack(pady=10)
+
+    # =========================
+    # RESULT LABEL
+    # =========================
+    result_label = ctk.CTkLabel(
+        main_scroll,
+        text="Area = ",
+        font=("Arial", 24, "bold")
+    )
+
+    result_label.pack(pady=20)
+
+    # =========================
+    # UPDATE PLACEHOLDER
+    # =========================
+    def update_placeholders():
+
         
