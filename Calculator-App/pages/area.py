37aee95d380a3a088e8cbb7a1c7564c617514f7c
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-05-23 12:50:33 +0530

    update

--- Calculator-App/pages/area.py
+++ Calculator-App/pages/area.py
@@ -178,3 +178,47 @@
 
 
 
+import customtkinter as ctk
+from database import save_history
+
+def create_area_converter(content):
+
+    # =========================
+    # CLEAR PAGE
+    # =========================
+    for widget in content.winfo_children():
+        widget.destroy()
+
+    # =========================
+    # MAIN FRAME
+    # =========================
+    main_frame = ctk.CTkFrame(
+        content,
+        fg_color="transparent"
+    )
+
+    main_frame.pack(
+        fill="both",
+        expand=True,
+        padx=15,
+        pady=15
+    )
+
+    # =========================
+    # AREA UNITS
+    # =========================
+    area_units = {
+
+        "Square millimeters": 1000000,
+        "Square centimeters": 10000,
+        "Square meters": 1,
+        "Hectares": 0.0001,
+        "Square kilometers": 0.000001,
+        "Square inches": 1550.0031,
+        "Square feet": 10.7639,
+        "Square yards": 1.19599,
+        "Acres": 0.000247105,
+        "Square miles": 0.000000386102
+    }
+
+   
