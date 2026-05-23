0feceacd3f5291a3256ea2e40795fff6a9d014f6
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-05-23 12:54:55 +0530

    update

--- Calculator-App/pages/bmi_calculator.py
+++ Calculator-App/pages/bmi_calculator.py
@@ -134,4 +134,116 @@ def bmi_calculator(content):
         pady=5
     )
 
+    # =========================
+    # WEIGHT ENTRY
+    # =========================
+    weight = ctk.CTkEntry(
+        display_frame,
+        height=55,
+        font=("Arial", 22, "bold"),
+        justify="right",
+        placeholder_text="Enter Weight"
+    )
+
+    weight.pack(
+        fill="x",
+        padx=10,
+        pady=(10, 5)
+    )
+
+    # =========================
+    # HEIGHT ENTRY
+    # =========================
+    height = ctk.CTkEntry(
+        display_frame,
+        height=55,
+        font=("Arial", 22, "bold"),
+        justify="right",
+        placeholder_text="Enter Height"
+    )
+
+    height.pack(
+        fill="x",
+        padx=10,
+        pady=(5, 10)
+    )
+
+    # =========================
+    # WEIGHT UNIT
+    # =========================
+    weight_unit = ctk.StringVar(
+        value="Kilogram"
+    )
+
+    weight_menu = ctk.CTkComboBox(
+        main_frame,
+        values=["Kilogram", "Pounds"],
+        variable=weight_unit,
+        height=45,
+        font=("Arial", 16, "bold"),
+        state="readonly"
+    )
+
+    weight_menu.pack(
+        fill="x",
+        padx=10,
+        pady=5
+    )
+
+    # =========================
+    # HEIGHT UNIT
+    # =========================
+    height_unit = ctk.StringVar(
+        value="Meters"
+    )
+
+    height_menu = ctk.CTkComboBox(
+        main_frame,
+        values=["Meters", "Centimeters", "Feet", "Inches"],
+        variable=height_unit,
+        height=45,
+        font=("Arial", 16, "bold"),
+        state="readonly"
+    )
+
+    height_menu.pack(
+        fill="x",
+        padx=10,
+        pady=5
+    )
+
+    # =========================
+    # RESULT FRAME
+    # =========================
+    result_frame = ctk.CTkFrame(
+        main_frame,
+        corner_radius=15,
+        fg_color="#1E293B"
+    )
+
+    result_frame.pack(
+        fill="x",
+        padx=10,
+        pady=15
+    )
+
+    ctk.CTkLabel(
+        result_frame,
+        text="BMI Result",
+        font=("Arial", 16)
+    ).pack(
+        pady=(10, 3)
+    )
+
+    result = ctk.CTkLabel(
+        result_frame,
+        text="0",
+        font=("Arial", 30, "bold"),
+        text_color="#38BDF8"
+    )
+
+    result.pack(
+        pady=(0, 10)
+    )
+
     
