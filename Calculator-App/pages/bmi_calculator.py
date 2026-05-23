b4344b7a79b0a0d0f596bc8b7c18f6cf3a709425
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-05-23 12:54:26 +0530

    update

--- Calculator-App/pages/bmi_calculator.py
+++ Calculator-App/pages/bmi_calculator.py
@@ -82,3 +82,56 @@
 
 
 
+import customtkinter as ctk
+from database import save_history
+
+def clear_content(content):
+
+    for widget in content.winfo_children():
+        widget.destroy()
+
+def bmi_calculator(content):
+
+    clear_content(content)
+
+    # =========================
+    # MAIN FRAME
+    # =========================
+    main_frame = ctk.CTkFrame(
+        content,
+        fg_color="transparent"
+    )
+
+    main_frame.pack(
+        fill="both",
+        expand=True,
+        padx=10,
+        pady=10
+    )
+
+    # =========================
+    # TITLE
+    # =========================
+    ctk.CTkLabel(
+        main_frame,
+        text="⚖️ BMI Calculator",
+        font=("Arial", 28, "bold")
+    ).pack(
+        pady=(5, 15)
+    )
+
+    # =========================
+    # DISPLAY FRAME
+    # =========================
+    display_frame = ctk.CTkFrame(
+        main_frame,
+        corner_radius=15
+    )
+
+    display_frame.pack(
+        fill="x",
+        padx=10,
+        pady=5
+    )
+
+    
