178d28ecaf445b354e926ce92cc6129b35e048d8
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-05-25 19:50:34 +0530

    update

--- Calculator-App/pages/discount_calculator.py
+++ Calculator-App/pages/discount_calculator.py
@@ -84,4 +84,70 @@ def discount_calculator(content):
         pady=(8, 15)
     )
 
+    # =========================
+    # RESULT FRAME
+    # =========================
+    result_frame = ctk.CTkFrame(
+        main_frame,
+        corner_radius=18,
+        fg_color="#1E293B"
+    )
+
+    result_frame.pack(
+        fill="x",
+        padx=20,
+        pady=20
+    )
+
+    ctk.CTkLabel(
+        result_frame,
+        text="Final Price",
+        font=("Arial", 16)
+    ).pack(
+        pady=(10, 5)
+    )
+
+    result = ctk.CTkLabel(
+        result_frame,
+        text="₹0",
+        font=("Arial", 32, "bold"),
+        text_color="#38BDF8"
+    )
+
+    result.pack(
+        pady=(0, 15)
+    )
+
+    # =========================
+    # ACTIVE ENTRY
+    # =========================
+    active_entry = price
+
+    def set_price_active(event):
+
+        nonlocal active_entry
+
+        active_entry = price
+
+    def set_discount_active(event):
+
+        nonlocal active_entry
+
+        active_entry = discount
+
+    price.bind("<FocusIn>", set_price_active)
+
+    discount.bind("<FocusIn>", set_discount_active)
+
+    # =========================
+    # BUTTON CLICK
+    # =========================
+    def button_click(value):
+
+        current = active_entry.get()
+
+        active_entry.delete(0, "end")
+
+        active_entry.insert(0, current + str(value))
+
     
