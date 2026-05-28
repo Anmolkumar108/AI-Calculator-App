f4797b08e9454b8670b04fd741f0e50b09a18d85
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-05-28 12:01:02 +0530

    update

--- Calculator-App/pages/gst_calculator.py
+++ Calculator-App/pages/gst_calculator.py
@@ -18,4 +18,47 @@ def gst_calculator(content):
 
     clear_content(content)
 
+    # =========================
+    # MAIN SCROLL FRAME
+    # =========================
+
+    scroll = ctk.CTkScrollableFrame(
+        content,
+        fg_color="transparent"
+    )
+
+    scroll.pack(
+        fill="both",
+        expand=True,
+        padx=10,
+        pady=10
+    )
+
+    # =========================
+    # TITLE
+    # =========================
+
+    ctk.CTkLabel(
+        scroll,
+        text="💰 GST Calculator",
+        font=("Arial", 30, "bold")
+    ).pack(
+        pady=(10, 20)
+    )
+
+    # =========================
+    # DISPLAY FRAME
+    # =========================
+
+    display_frame = ctk.CTkFrame(
+        scroll,
+        corner_radius=20
+    )
+
+    display_frame.pack(
+        fill="x",
+        padx=15,
+        pady=10
+    )
+
     
