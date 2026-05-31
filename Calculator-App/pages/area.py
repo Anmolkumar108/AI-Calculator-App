6a9682521f59e8a251d5e154a4a032d841617e51
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-05-31 22:33:07 +0530

    update

--- Calculator-App/pages/area.py
+++ Calculator-App/pages/area.py
@@ -1,37 +1,5 @@
 
-#     # =========================
-#     # MAIN FRAME
-#     # =========================
-#     main_frame = ctk.CTkFrame(
-#         content,
-#         fg_color="transparent"
-#     )
-
-#     main_frame.pack(
-#         fill="both",
-#         expand=True,
-#         padx=20,
-#         pady=20
-#     )
-
-#     # =========================
-#     # AREA UNITS
-#     # Base Unit = Square Meter
-#     # =========================
-#     area_units = {
-
-#         "Square millimeters": 1000000,
-#         "Square centimeters": 10000,
-#         "Square meters": 1,
-#         "Hectares": 0.0001,
-#         "Square kilometers": 0.000001,
-#         "Square inches": 1550.0031,
-#         "Square feet": 10.7639,
-#         "Square yards": 1.19599,
-#         "Acres": 0.000247105,
-#         "Square miles": 0.000000386102
-#     }
-
+#     
 #     # =========================
 #     # TITLE
 #     # =========================
