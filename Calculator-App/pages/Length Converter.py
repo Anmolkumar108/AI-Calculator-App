26e4105839c13adfb69e8cdcf047f3389cd7643c
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-06-01 22:05:11 +0530

    update

--- Calculator-App/pages/Length Converter.py
+++ Calculator-App/pages/Length Converter.py
@@ -10,3 +10,40 @@ def clear_content(content):
     for widget in content.winfo_children():
         widget.destroy()
 
+# =========================
+# LENGTH CONVERTER PAGE
+# =========================
+
+def length_converter(content):
+
+    clear_content(content)
+
+    # =========================
+    # MAIN SCROLL FRAME
+    # =========================
+
+    scroll = ctk.CTkScrollableFrame(
+        content,
+        fg_color="transparent"
+    )
+
+    scroll.pack(
+        fill="both",
+        expand=True,
+        padx=10,
+        pady=10
+    )
+
+    # =========================
+    # TITLE
+    # =========================
+
+    ctk.CTkLabel(
+        scroll,
+        text="📏 Advanced Length Converter",
+        font=("Arial", 30, "bold")
+    ).pack(
+        pady=(10, 20)
+    )
+
+    
