7660c15255b0ac5f5a59eedaae2a053869847583
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-06-02 14:21:45 +0530

    update

--- Calculator-App/pages/normal_calculator.py
+++ Calculator-App/pages/normal_calculator.py
@@ -9,3 +9,52 @@ def clear_content(content):
     for widget in content.winfo_children():
         widget.destroy()
 
+# =========================
+# NORMAL CALCULATOR
+# =========================
+
+def normal_calculator(content):
+    clear_content(content)
+
+    scroll_frame = ctk.CTkScrollableFrame(
+        content,
+        fg_color="transparent"
+    )
+
+    scroll_frame.pack(
+        fill="both",
+        expand=True,
+        padx=10,
+        pady=10
+    )
+
+    main_frame = ctk.CTkFrame(
+        scroll_frame,
+        fg_color="transparent"
+    )
+
+    main_frame.pack(
+        fill="both",
+        expand=True,
+        padx=15,
+        pady=15
+    )
+
+    ctk.CTkLabel(
+        main_frame,
+        text="🧮 Normal Calculator",
+        font=("Arial", 30, "bold")
+    ).pack(pady=(10, 20))
+
+    display_frame = ctk.CTkFrame(
+        main_frame,
+        corner_radius=18
+    )
+
+    display_frame.pack(
+        fill="x",
+        padx=15,
+        pady=10
+    )
+
+   
