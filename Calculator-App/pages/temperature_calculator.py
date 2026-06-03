8ee931a4b0a1127ba84de36112de79c0e5fa62cc
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-06-03 15:32:42 +0530

    update

--- Calculator-App/pages/temperature_calculator.py
+++ Calculator-App/pages/temperature_calculator.py
@@ -17,4 +17,47 @@ def temperature_calculator(content):
     # ==========================================
     memory_value = 0
 
+    # ==========================================
+    # MAIN FRAME
+    # ==========================================
+    main_frame = ctk.CTkFrame(
+        content,
+        fg_color="#1b1b1b"
+    )
+    main_frame.pack(
+        fill="both",
+        expand=True
+    )
+
+    # ==========================================
+    # TITLE
+    # ==========================================
+    title = ctk.CTkLabel(
+        main_frame,
+        text="🌡️ Temperature Converter",
+        font=("Arial", 30, "bold")
+    )
+    title.pack(
+        anchor="w",
+        padx=20,
+        pady=(15, 10)
+    )
+
+    # ==========================================
+    # DISPLAY
+    # ==========================================
+    display = ctk.CTkEntry(
+        main_frame,
+        height=80,
+        font=("Arial", 35),
+        justify="right",
+        border_width=0,
+        fg_color="#1b1b1b"
+    )
+    display.pack(
+        fill="x",
+        padx=20,
+        pady=(0, 15)
+    )
+
     
