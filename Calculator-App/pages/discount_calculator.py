8ecbba4ce6d64364cbb4f4e43b91b178a26d3a28
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-06-06 13:30:09 +0530

    update

--- Calculator-App/pages/discount_calculator.py
+++ Calculator-App/pages/discount_calculator.py
@@ -25,4 +25,46 @@ def discount_calculator(content):
         pady=15
     )
 
+    # =========================
+    # TITLE
+    # =========================
+    ctk.CTkLabel(
+        main_frame,
+        text="🏷️ Discount Calculator",
+        font=("Arial", 30, "bold")
+    ).pack(
+        pady=(10, 20)
+    )
+
+    # =========================
+    # DISPLAY FRAME
+    # =========================
+    display_frame = ctk.CTkFrame(
+        main_frame,
+        corner_radius=18
+    )
+
+    display_frame.pack(
+        fill="x",
+        padx=15,
+        pady=10
+    )
+
+    # =========================
+    # PRICE ENTRY
+    # =========================
+    price = ctk.CTkEntry(
+        display_frame,
+        height=55,
+        font=("Arial", 22, "bold"),
+        justify="right",
+        placeholder_text="Enter Price"
+    )
+
+    price.pack(
+        fill="x",
+        padx=15,
+        pady=(15, 8)
+    )
+
     
