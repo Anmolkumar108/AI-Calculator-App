900a4e7d7533469ce23d18a4030bf1f786119d82
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-06-07 14:07:59 +0530

    update

--- Calculator-App/pages/bmi_calculator.py
+++ Calculator-App/pages/bmi_calculator.py
@@ -373,4 +373,18 @@ def bmi_calculator(content):
                 hover_color="darkred"
             )
 
-        
+        elif text == "⌫":
+
+            btn = ctk.CTkButton(
+                keypad_frame,
+                text=text,
+                command=backspace,
+                width=50,
+                height=50,
+                font=("Arial", 15, "bold"),
+                corner_radius=12,
+                fg_color="orange",
+                hover_color="darkorange"
+            )
+
+       
