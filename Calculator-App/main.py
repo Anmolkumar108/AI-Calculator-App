5b4d1c20757501c94ebf88734ba1b044539807a8
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-06-14 13:47:44 +0530

    update

--- Calculator-App/main.py
+++ Calculator-App/main.py
@@ -6,17 +6,6 @@
 # # from pages.gst_calculator import gst_calculator
 # # from pages.discount_calculator import discount_calculator
 # # from pages.bmi_calculator import bmi_calculator
-# # from pages.dob_calculator import dob_calculator
-# # from pages.currency_converter import currency_converter
-# # from pages.unit_converter import unit_converter
-# # from pages.history_page import show_history
-# # from pages.ai_section import ai_section
-
-# # from theme import dark_theme, light_theme
-
-# # # =========================
-# # # APP SETTINGS
-# # # =========================
 
 
 import customtkinter as ctk
