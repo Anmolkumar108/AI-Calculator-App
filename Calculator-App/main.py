4a69263c801644b6cec580e8e8a75d18d1a84946
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-07-22 11:19:40 +0530

    Update

--- Calculator-App/main.py
+++ Calculator-App/main.py
@@ -1,10 +1,3 @@
-import customtkinter as ctk
-
-from pages.normal_calculator import normal_calculator
-from pages.scientific_calculator import scientific_calculator
-from pages.gst_calculator import gst_calculator
-from pages.discount_calculator import discount_calculator
-from pages.bmi_calculator import bmi_calculator
 
 
 
