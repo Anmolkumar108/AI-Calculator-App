a4b5cc83b9204b517cbb1b8acb5373f3e398ac19
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-07-23 00:11:40 +0530

    Your commit message

--- Calculator-App/main.py
+++ Calculator-App/main.py
@@ -11,4 +11,10 @@ def load_page_module(module_name):
     if module_name in _module_cache:
         return _module_cache[module_name]
 
-    
+    if module_name == "length_converter":
+        module_path = os.path.join(os.path.dirname(__file__), "pages", "Length Converter.py")
+        spec = importlib.util.spec_from_file_location("length_converter_module", module_path)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+    else:
+        
