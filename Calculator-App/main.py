19cf62f6b7fd8f95e560132d3d5a082a8f1ba283
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-07-23 00:11:25 +0530

    Your commit message

--- Calculator-App/main.py
+++ Calculator-App/main.py
@@ -7,3 +7,8 @@ from theme import dark_theme, light_theme
 
 _module_cache = {}
 
+def load_page_module(module_name):
+    if module_name in _module_cache:
+        return _module_cache[module_name]
+
+    
