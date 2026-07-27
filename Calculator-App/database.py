0e1e365979adeab03eff879cb782e4f04ec47d7f
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-07-27 12:36:12 +0530

    update

--- Calculator-App/database.py
+++ Calculator-App/database.py
@@ -8,3 +8,25 @@ conn = sqlite3.connect("database.db")
 
 cur = conn.cursor()
 
+# =========================
+# CREATE TABLE
+# =========================
+
+cur.execute("""
+
+CREATE TABLE IF NOT EXISTS history (
+
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+    calculation TEXT,
+
+    timestamp TEXT,
+
+    page TEXT
+
+)
+
+""")
+
+conn.commit()
+
