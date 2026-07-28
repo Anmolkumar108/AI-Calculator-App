42b4843856db913718f0b177b49387cc3f4d3e8a
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-07-28 15:53:48 +0530

    Update

--- Calculator-App/pages/ai_section.py
+++ Calculator-App/pages/ai_section.py
@@ -19,4 +19,30 @@ def ai_section(content):
     )
     title.pack(pady=20)
 
+    chat_box = ctk.CTkTextbox(
+        content,
+        width=850,
+        height=450,
+        font=('Arial', 16),
+        corner_radius=15
+    )
+    chat_box.pack(pady=10)
+
+    chat_box.insert(
+        'end',
+        '🤖 AI: Hello Anmol 👋\n'
+        'I can solve advanced math problems.\n\n'
+        'Examples:\n'
+        '5+8*2\n'
+        'sqrt 25\n'
+        'sin 90\n'
+        'log 100\n'
+        'factorial 5\n'
+        'x+5=10\n'
+        'integrate x**2\n'
+        'differentiate x**3\n'
+        'expand (x+2)^2\n'
+        'simplify (x^2+2x+1)\n\n'
+    )
+
     
