0b9818c74f318177be7c8159abb9382fd95b0eb7
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-07-30 14:55:36 +0530

    Update

--- Calculator-App/pages/area.py
+++ Calculator-App/pages/area.py
@@ -24,4 +24,32 @@ def create_area_converter(content, restore=None):
         pady=15
     )
 
+    # =========================
+    # AREA UNITS
+    # =========================
+    area_units = {
+
+        "Square millimeters": 1000000,
+        "Square centimeters": 10000,
+        "Square meters": 1,
+        "Hectares": 0.0001,
+        "Square kilometers": 0.000001,
+        "Square inches": 1550.0031,
+        "Square feet": 10.7639,
+        "Square yards": 1.19599,
+        "Acres": 0.000247105,
+        "Square miles": 0.000000386102
+    }
+
+    # =========================
+    # TITLE
+    # =========================
+    ctk.CTkLabel(
+        main_frame,
+        text="📐 Area Converter",
+        font=("Arial", 35, "bold")
+    ).pack(
+        pady=(10, 20)
+    )
+
     
