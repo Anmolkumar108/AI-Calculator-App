bba653d0075feda6552b1dd5e1758213afa56b82
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-07-30 14:55:02 +0530

    Update

--- Calculator-App/pages/area.py
+++ Calculator-App/pages/area.py
@@ -3,4 +3,25 @@ from database import save_history
 
 def create_area_converter(content, restore=None):
 
+    # =========================
+    # CLEAR PAGE
+    # =========================
+    for widget in content.winfo_children():
+        widget.destroy()
+
+    # =========================
+    # MAIN FRAME
+    # =========================
+    main_frame = ctk.CTkScrollableFrame(
+        content,
+        fg_color="transparent"
+    )
+
+    main_frame.pack(
+        fill="both",
+        expand=True,
+        padx=15,
+        pady=15
+    )
+
     
