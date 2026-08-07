3dab6ab9e7becf242b11787f92865121ec65d95d
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-08-07 14:39:48 +0530

    Update

--- Calculator-App/pages/dob_calculator.py
+++ Calculator-App/pages/dob_calculator.py
@@ -2,3 +2,19 @@ import customtkinter as ctk
 from datetime import date
 from database import save_history
 
+# =========================
+# CLEAR OLD CONTENT
+# =========================
+def clear_content(content):
+
+    for widget in content.winfo_children():
+        widget.destroy()
+
+# =========================
+# DOB CALCULATOR
+# =========================
+def dob_calculator(content):
+
+    clear_content(content)
+
+    
