af3ebc3ab7fd69c57ab5647246617d2f4b34b6cb
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-08-08 13:50:48 +0530

    update

---
+++ Calculator-App/pages/gst_calculator.py
@@ -0,0 +1,12 @@
+import customtkinter as ctk
+from database import save_history
+
+# =========================
+# CLEAR CONTENT
+# =========================
+
+def clear_content(content):
+
+    for widget in content.winfo_children():
+        widget.destroy()
+
