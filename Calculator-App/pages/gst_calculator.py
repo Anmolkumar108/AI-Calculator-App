5c61bf5defd595c5873f98dddd5f2692f6fd27c1
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-08-08 13:54:46 +0530

    update

--- Calculator-App/pages/gst_calculator.py
+++ Calculator-App/pages/gst_calculator.py
@@ -388,4 +388,19 @@ def gst_calculator(content):
                 hover_color="#1D4ED8"
             )
 
+        # REMOVE GST BUTTON
+        elif text == "-":
+
+            btn = ctk.CTkButton(
+                keypad_frame,
+                text=text,
+                command=remove_gst,
+                width=55,
+                height=55,
+                font=("Arial", 20, "bold"),
+                corner_radius=14,
+                fg_color="orange",
+                hover_color="darkorange"
+            )
+
         
