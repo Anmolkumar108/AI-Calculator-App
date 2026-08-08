8ddfd98c6126835b4526d48c004f601862f5b2f1
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-08-08 13:51:21 +0530

    update

--- Calculator-App/pages/gst_calculator.py
+++ Calculator-App/pages/gst_calculator.py
@@ -10,3 +10,28 @@ def clear_content(content):
     for widget in content.winfo_children():
         widget.destroy()
 
+# =========================
+# GST CALCULATOR
+# =========================
+
+def gst_calculator(content):
+
+    clear_content(content)
+
+    # =========================
+    # MAIN SCROLL FRAME
+    # =========================
+
+    scroll = ctk.CTkScrollableFrame(
+        content,
+        fg_color="transparent"
+    )
+
+    scroll.pack(
+        fill="both",
+        expand=True,
+        padx=10,
+        pady=10
+    )
+
+    
