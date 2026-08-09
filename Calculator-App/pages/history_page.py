24c29b1220345df252dd2b0a413ed6d11278e7fc
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-08-09 11:10:45 +0530

    update

--- Calculator-App/pages/history_page.py
+++ Calculator-App/pages/history_page.py
@@ -4,3 +4,15 @@ import importlib.util
 import customtkinter as ctk
 from database import get_history
 
+# import page render functions so we can navigate back
+from pages.normal_calculator import normal_calculator
+from pages.scientific_calculator import scientific_calculator
+from pages.gst_calculator import gst_calculator
+from pages.discount_calculator import discount_calculator
+from pages.bmi_calculator import bmi_calculator
+from pages.dob_calculator import dob_calculator
+from pages.currency_converter import currency_converter
+from pages.unit_converter import unit_converter
+from pages.temperature_calculator import temperature_calculator
+from pages.area import create_area_converter as area
+
